778585ea609d3fa60a9329bec785fba0413ebe5d
wedul <wedul@wedul>
2017-06-25 11:55:35 +0900

    0

--- src/main/front/login/login.js
+++ src/main/front/login/login.js
@@ -2,5 +2,9 @@ import $ from '../jslib/jquery.1.11.2.min.js'
 import common from '../jslib/commonUtil'
 
 window.onload = function() {
+	var $id = $('#textId');
+	
+	$id.text("dfdfdf");
+	
   alert('ddfdsfad');
 };
